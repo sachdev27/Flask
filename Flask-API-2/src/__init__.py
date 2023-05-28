600016f6ab0f2156e6d1f1d6e385369e6d597cfb
sandeshsachdev <sandeshsachdev27@gmail.com>
2023-05-28 14:13:03 +0530

    login in Auth

--- Flask-API-2/src/__init__.py
+++ Flask-API-2/src/__init__.py
@@ -3,6 +3,7 @@ import os
 from src.database import db
 from src.auth import auth
 from src.bookmarks import bookmarks
+from flask_jwt_extended import JWTManager
 
 # Create the application factory function
 def create_app(test_config=None):
@@ -13,7 +14,8 @@ def create_app(test_config=None):
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY = os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+            SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
         )
     else:
         app.config.from_mapping(test_config)
@@ -32,6 +34,11 @@ def create_app(test_config=None):
     # Initialize the database
     db.app = app
     db.init_app(app)
+    
+    
+    # JWT Manager
+    # JWTManager is used to handle the creation and verification of JSON Web Tokens.
+    JWTManager(app)
     
     # What are Blue print ?
     # Blueprints are a way to organize related routes in a Flask application.
--- Flask-API-2/src/auth.py
+++ Flask-API-2/src/auth.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from src.constants.http_status_codes import *
 import validators
 from src.database import User,db
+from flask_jwt_extended import create_access_token,create_refresh_token
 
 auth = Blueprint("auth",__name__,url_prefix="/api/v1/auth")
 
@@ -54,8 +55,6 @@ def register():
     # Saving the change in database
     db.session.commit()
     
-    
-    
     return jsonify({
         "message" : "User is created",
         "user" : {
@@ -65,9 +64,34 @@ def register():
     }), HTTP_201_CREATED
     
     
+@auth.post('/login')
+def login():
+    email = request.json.get('email',"")
+    password = request.json.get('password',"")
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if user:
+        # Checking password strored in Database in hashed format with user's given credential
+        is_pass_correct = check_password_hash(pwhash=user.password,password=password)   
         
+        if is_pass_correct:
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
 
-
+            return jsonify({
+                "access" : access,
+                "refresh": refresh,
+                "username" : user.username,
+                "email" : user.email                
+            }),HTTP_200_OK
+        
+        else:
+            return jsonify({
+                "error": "Invalid Username or Password"
+            }),HTTP_401_UNAUTHORIZED
+            
+            
 
 @auth.get("/me") 
 def me():
