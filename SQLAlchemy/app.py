699cec27e12df095b29381fc58afed94c720376c
sandeshsachdev <sandeshsachdev27@gmail.com>
2023-06-08 17:46:18 +0530

    Initial Commit

---
+++ SQLAlchemy/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET'])
+def index():
+    return "HELLO WORLD"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
